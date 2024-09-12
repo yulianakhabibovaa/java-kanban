ffd7c600aafa45c4c2f9c3e4e7f37ef3fa4ccf1d
yulianakhabibovaa <yulianakhabibovaa@yandex.ru>
2024-09-12 19:06:35 +0300

    Попытка исправить проверки

--- test/manager/ManagersTest.java
+++ test/manager/ManagersTest.java
@@ -10,7 +10,8 @@ import tasks.Task;
 import java.util.ArrayList;
 import java.util.List;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertIterableEquals;
 
 public class ManagersTest {
 
